5bb27e65fb6bb5a581cc8e8bdb43d3e4456f0354
Nite Luo <darkdukey@gmail.com>
2015-08-13 14:56:16 -0700

    sdkbox import adcolony

--- cpp/Classes/AppDelegate.cpp
+++ cpp/Classes/AppDelegate.cpp
@@ -1,5 +1,6 @@
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
+#include "PluginAdColony/PluginAdColony.h"
 
 USING_NS_CC;
 
@@ -35,6 +36,9 @@ static int register_all_packages()
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
+
+    sdkbox::PluginAdColony::init();
+
     // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
