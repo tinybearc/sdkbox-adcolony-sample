495060a9894cc47aa374f4d2acb16c12caee30f0
yinjimmy <jimmy.yin5@gmail.com>
2015-11-10 12:34:00 +0800

    Fix adcolony callback

--- cpp/Classes/HelloWorldScene.cpp
+++ cpp/Classes/HelloWorldScene.cpp
@@ -15,7 +15,7 @@ Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
-    
+
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
 
@@ -30,35 +30,35 @@ Scene* HelloWorld::createScene()
 bool HelloWorld::init()
 {
     _coin = 0;
-    
+
     //////////////////////////////
     // 1. super init first
     if ( !Layer::init() )
     {
         return false;
     }
-    
+
     FileUtils::getInstance()->addSearchPath("res");
-    
+
     sdkbox::PluginAdColony::init();
     sdkbox::PluginAdColony::setListener(this);
 
     auto rootNode = CSLoader::createNode("MainScene.csb");
     addChild(rootNode);
-    
+
     _btnVideo = rootNode->getChildByName<ui::Button*>("btnVideo");
     _btnVideo->addClickEventListener(CC_CALLBACK_1(HelloWorld::onPlayVideo, this));
     _btnVideo->setBright(false);
-    
+
     _btnReward = rootNode->getChildByName<ui::Button*>("btnReward");
     _btnReward->addClickEventListener(CC_CALLBACK_1(HelloWorld::onPlayReward, this));
     _btnReward->setBright(false);
-    
+
     _txtCoin = rootNode->getChildByName<ui::Text*>("txtCoins");
-    
+
     auto btnClose = rootNode->getChildByName<ui::Button*>("btnClose");
     btnClose->addClickEventListener(CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
+
     return true;
 }
 
@@ -85,32 +85,36 @@ void HelloWorld::menuCloseCallback(Ref* pSender)
 #endif
 }
 
-/** 
+/**
  * AdColony Callback
  */
 void HelloWorld::onAdColonyChange(const sdkbox::AdColonyAdInfo& info, bool available)
 {
-    CCLOG("Finish loading ads %s", info.name.c_str());
+    CCLOG("[adcolony] Finish loading ads %s", info.name.c_str());
     _btnVideo->setBright(true);
     _btnReward->setBright(true);
 }
 
 void HelloWorld::onAdColonyReward(const sdkbox::AdColonyAdInfo& info, const std::string& currencyName, int amount, bool success)
 {
+    CCLOG("[adcolony] onAdColonyReward");
     if (success)
     {
         //
         _coin ++;
-        _txtCoin->setString(to_string(_coin));
+
+         Director::getInstance()->getScheduler()->performFunctionInCocosThread([=](){
+            _txtCoin->setString(to_string(_coin));
+         });
     }
 }
 
 void HelloWorld::onAdColonyStarted(const sdkbox::AdColonyAdInfo& info)
 {
-    CCLOG("Ad started: %s", info.name.c_str());
+    CCLOG("[adcolony] Ad started: %s", info.name.c_str());
 }
 
 void HelloWorld::onAdColonyFinished(const sdkbox::AdColonyAdInfo& info)
 {
-    CCLOG("Ad ended: %s", info.name.c_str());
+    CCLOG("[adcolony] Ad ended: %s", info.name.c_str());
 }
